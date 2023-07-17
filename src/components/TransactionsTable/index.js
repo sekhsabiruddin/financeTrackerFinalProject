ccd3860e341f10269d027ee90d799de9acd8e1f2
sabir <sksabiruddin81@gmail.com>
2023-07-17 21:21:20 +0530

    fixed-the-table-issue

--- src/components/TransactionsTable/index.js
+++ src/components/TransactionsTable/index.js
@@ -18,56 +18,52 @@ const TransactionsTable = ({ transactions }) => {
       title: "Name",
       dataIndex: "name",
       key: "name",
+      fixed: "left", // Make the column fixed to the left
+      width: 120, // Set a fixed width for the column
     },
     {
       title: "Amount",
       dataIndex: "amount",
       key: "amount",
+      fixed: "left", // Make the column fixed to the left
+      width: 120, // Set a fixed width for the column
     },
     {
       title: "Tag",
       dataIndex: "tag",
       key: "tag",
+      width: 130,
     },
     {
       title: "Type",
       dataIndex: "type",
       key: "type",
+      width: 130,
     },
     {
       title: "Date",
       dataIndex: "date",
       key: "date",
+      width: 130,
     },
     {
       title: "Actions",
       key: "actions",
-
+      width: 130,
       render: () => (
         <Space>
-          <EditOutlined style={{ color: "green" }} />
-          <DeleteOutlined style={{ color: "red", marginLeft: "10px" }} />
+          <EditOutlined style={{ color: "green", cursor: "pointer" }} />
+          <DeleteOutlined
+            style={{ color: "red", marginLeft: "10px", cursor: "pointer" }}
+          />
         </Space>
       ),
     },
   ];
-  //it is for search and type
-  const filteredTransactions = transactions.filter(
-    (item) =>
-      item.name.toLowerCase().includes(search.toLowerCase()) &&
-      (typeFilter === "" || item.type === typeFilter)
-  );
-  //it use for sorting
-  let sortedTransactions = filteredTransactions.sort((a, b) => {
-    if (sortKey === "date") {
-      return new Date(a.date) - new Date(b.date);
-    } else if (sortKey === "amount") {
-      return a.amount - b.amount;
-    } else {
-      return 0;
-    }
-  });
-  //function exportCSV
+
+  // ...rest of the code
+
+  // Function for exporting to CSV
   function exportCSV() {
     var csv = Papa.unparse({
       fields: ["name", "type", "tag", "date", "amount"],
@@ -93,10 +89,26 @@ const TransactionsTable = ({ transactions }) => {
     },
   };
 
+  const filteredTransactions = transactions.filter(
+    (item) =>
+      item.name.toLowerCase().includes(search.toLowerCase()) &&
+      (typeFilter === "" || item.type === typeFilter)
+  );
+
+  let sortedTransactions = filteredTransactions.sort((a, b) => {
+    if (sortKey === "date") {
+      return new Date(a.date) - new Date(b.date);
+    } else if (sortKey === "amount") {
+      return a.amount - b.amount;
+    } else {
+      return 0;
+    }
+  });
+
   return (
-    <div style={{ width: "100%", padding: "0rem 2rem" }}>
+    <div style={{ width: "100%" }}>
       <div
-        className="my-table-filter"
+        className="-filter"
         style={{
           display: "flex",
           justifyContent: "space-between",
@@ -155,7 +167,6 @@ const TransactionsTable = ({ transactions }) => {
             onChange={(value) => setTypeFilter(value)}
             value={typeFilter}
             placeholder="Filter"
-            // allowClear
           >
             <Option value="">All</Option>
             <Option value="income">Income</Option>
@@ -168,6 +179,7 @@ const TransactionsTable = ({ transactions }) => {
             dataSource={sortedTransactions}
             columns={columns}
             components={components}
+            scroll={{ x: 500 }} // Set a horizontal scroll for the table
           />
         </div>
       </div>
